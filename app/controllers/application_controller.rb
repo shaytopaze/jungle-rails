8058808903c5ed0bf50afd83a0fbf442a0696f19
Shay Alexander <shaytopazehair@gmail.com>
2018-03-01 02:46:01 +0000

    added basic http authentification to admin pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,10 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  # def basic
+  #   http_basic_authenticate_with :name => "jungle", :password => "book"
+  # end
+
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
@@ -11,6 +15,7 @@ class ApplicationController < ActionController::Base
   def authorize
     redirect_to '/login' unless current_user
   end
+
 
   private
 
