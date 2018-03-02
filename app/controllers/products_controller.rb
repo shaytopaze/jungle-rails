41874d645ccf1b1a81579eb423062c620553578b
Shay Alexander <shaytopazehair@gmail.com>
2018-03-02 05:54:59 +0000

    added average rating to products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,13 +2,16 @@ class ProductsController < ApplicationController
 
   def index
     @products = Product.all.order(created_at: :desc)
-    @reviews = Review.all.order(created_at: :desc)
+    # @reviews = Review.all.order(created_at: :desc)
   end
 
   def show
     @product = Product.find params[:id]
     @reviews = Review.find_by_product_id params[:id]
     @review = Review.new(reviews_params)
+    if @reviews
+      @average_rating = Review.where(product_id: params[:id]).average(:rating).round(2)
+    end
   end
 
   private
