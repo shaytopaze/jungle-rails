9904534d3faaecabb326afa1da15d37a815bfc2e
Shay Alexander <shaytopazehair@gmail.com>
2018-03-02 03:32:02 +0000

    added feature so that only logged in users can create a review, and users can only delete there own reviews

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,7 +3,6 @@ class OrdersController < ApplicationController
   def show
     @current_user = User.find(session[:user_id])
     @order = Order.find(params[:id])
-    # UserMailer.welcome_email(@current_user, @order).deliver_now
   end
 
   def create
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,48 @@
 class ReviewsController < ApplicationController
+
+before_action :authorize
+
+def new
+    @review = Review.new
+  end
+
+  def create
+    @product = Product.find(params[:product_id].to_i)
+    rating = params[:review][:rating]
+    description = params[:review][:description]
+    if rating && description
+      @review = Review.new(product_id: @product.id, user_id: current_user.id, rating: rating.to_i, description: description)
+      @review.save
+      if @review.save
+        flash[:notice] = "Your review has been submitted successfully"
+        redirect_to @product
+      else
+        flash[:error] = "There was an error deleteing the purchase."
+        render @product
+      end
+    end
+  end
+
+  def destroy
+    @product = Product.find(params[:product_id])
+    @review = Review.find(params[:id])
+    if @review.destroy
+      flash[:notice] = "Your review was deleted successfully."
+      redirect_to @product
+    else
+      flash[:error] = "There was an error deleteing the purchase."
+      render :show
+    end
+  end
+
+  private
+
+  def reviews_params
+    params.permit(
+      :description,
+      :rating,
+      :product_id,
+      :user_id
+    )
+  end
 end
