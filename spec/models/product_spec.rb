38d4cdf9e53883ca65f1b6d417b5e84a183f8e7c
Shay Alexander <shaytopazehair@gmail.com>
2018-03-05 20:17:04 +0000

    created model spec: User

---
+++ spec/models/product_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+RSpec.describe Product, type: :model do
+  describe 'Validations' do
+    before :example do
+      @category = Category.create(name:'test')
+      @product = @category.products.create(name: 'product', price_cents: 10, quantity: 1)
+    end
+    it "creates a product with a name, price, quantity and category" do
+      expect(@product.valid?).to eq true
+    end
+    it "throws error if name is missing" do
+      @product.name = nil
+      expect(@product.valid?).to eq false
+    end
+    it "throws error if price is missing" do
+      @product.price_cents = nil
+      expect(@product.valid?).to eq false
+    end
+    it "throws error if price is missing" do
+      @product.quantity = nil
+      expect(@product.valid?).to eq false
+    end
+    it "throws error if price is missing" do
+      @product.category = nil
+      expect(@product.valid?).to eq false
+    end
+  end
+end
+
